846135b6daa2d62f3fafb24544ec35a4a7c79c5f
Brant <uy.brantfranciss@gmail.com>
2021-06-07 21:38:38 +0800

    adding game files

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,16 @@
 import Phaser from 'phaser'
 
-import HelloWorldScene from './scenes/HelloWorldScene'
+import HelloWorldScene from './scenes'
 
-const config = {
+const config: Phaser.Types.Core.GameConfig = {
 	type: Phaser.AUTO,
 	width: 800,
 	height: 600,
 	physics: {
 		default: 'arcade',
 		arcade: {
-			gravity: { y: 200 }
+			gravity: { y: 300 },
+			debug: true
 		}
 	},
 	scene: [HelloWorldScene]
